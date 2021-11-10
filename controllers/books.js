71308329dd0f4c1c3e323e6e30fd2c86a57434b7
mattgefen <matt.gefen@gmail.com>
2021-11-10 10:36:25 -0700

    book edit and update

--- controllers/books.js
+++ controllers/books.js
@@ -3,7 +3,6 @@ import { Book } from "../models/book.js"
 async function index(req, res) {
   try {
     const books = await Book.find({},)
-    console.log(books)
     res.render('books/index', {
       title: 'Unit 2 Assessment',
       books
@@ -18,6 +17,30 @@ function newBook(req, res) {
     res.render('books/new', {
       title: 'Add a Book'
     })
+  } catch(error) {
+    console.log(error)
+    res.redirect('/books')
+  }
+}
+async function edit(req, res) {
+  try {
+    const book = await Book.findById(req.params.id)
+    res.render('books/edit', {
+      title: `Edit Book`,
+      book
+    })
+  } catch(error) {
+    console.log(error)
+    res.redirect('/books')
+  }
+}
+
+async function update(req, res) {
+  try {
+    let book = await Book.findById(req.params.id)
+    req.body.read = !!req.body.read
+    await book.update(req.body, {new: true})
+    res.redirect('/books')
   } catch(error) {
     console.log(error)
     res.redirect('/books')
@@ -47,5 +70,7 @@ export {
   index,
   newBook as new,
   create,
-  deleteBook as delete
+  deleteBook as delete,
+  edit,
+  update
 }
--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,9 @@ const router = Router()
 /* GET users listing. */
 router.get('/', booksCtrl.index)
 router.get('/new', booksCtrl.new)
+router.get('/:id/edit', booksCtrl.edit)
 router.post('/', booksCtrl.create)
+router.patch('/:id', booksCtrl.update)
 router.delete('/:id', booksCtrl.delete)
 
 export {
